fcebf0bdb766accb7f39742e977c9b83e74eb0fc
Żórawska, Nina (Stud. DACS) <n.zorawska@student.maastrichtuniversity.nl>
2024-03-20 14:19:32 +0000

    Update GUIOlaf.java

--- GUIOG.java
+++ GUIOG.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class GUIOlaf extends Application {
+public class GUIOG extends Application {
     private TextField dimensionsField = new TextField();
     private TextField startTimeField = new TextField();
     private TextField endTimeField = new TextField();

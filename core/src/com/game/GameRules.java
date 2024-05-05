3a8cb4e39f3116988ade0eaafa263b34361ae57a
Żórawska, Nina (Stud. DACS) <n.zorawska@student.maastrichtuniversity.nl>
2024-05-05 12:23:56 +0000

    added fellinwater method

--- core/src/com/game/GameRules.java
+++ core/src/com/game/GameRules.java
@@ -3,10 +3,13 @@ package com.game;
 public class GameRules {
     private Target target;
     private GolfBall ball;
+    private String functionTerrain;
 
-    public GameRules(Target target, GolfBall ball) {
+
+    public GameRules(Target target, GolfBall ball, String functionTerrain) {
         this.target = target;
         this.ball = ball;
+        this.functionTerrain = functionTerrain;
     }
 
     // Method to check if the game is over (i.e., ball reaches the target)
@@ -16,10 +19,28 @@ public class GameRules {
         return distance <= target.getRadius();
     }
 
+    //Method to check if ball is in water
+    public boolean fellInWater() {
+        double positionx = ball.getPosition().x;
+        double positiony = ball.getPosition().y;
+        double height = GetHeight.getHeight(functionTerrain, positionx, positiony); 
+        return height < 0;
+    }
+
+    //Method to check if ball fell out of border
+    // public boolean outOfBorder() {
+    //     double positionx = ball.getPosition().x;
+    //     double positiony = ball.getPosition().y;
+
+    // }
+
     // Display the game over message
     public void checkGameStatus() {
         if (isGameOver()) {
             System.out.println("Game Over! Ball has reached the target.");
+        } 
+        if (fellInWater()) {
+            System.out.println("Game Over! Ball fell into water.");
         }
     }
 }

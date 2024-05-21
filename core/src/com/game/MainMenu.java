773c2fcb7bc50f842da76e6e30087c617007c6ef
Żórawska, Nina (Stud. DACS) <n.zorawska@student.maastrichtuniversity.nl>
2024-05-21 09:15:23 +0000

    MainMenu

---
+++ core/src/com/game/MainMenu.java
@@ -0,0 +1,158 @@
+package com.GUI;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+
+
+public class MainMenu implements Screen {
+
+    // buttons
+    private static final int EXIT_BUTTON_WIDTH = 150;
+    private static final int EXIT_BUTTON_HEIGHT = 75;
+    private static final int PLAY_BUTTON_WIDTH = 300;
+    private static final int PLAY_BUTTON_HEIGHT = 150;
+    private static final int EXIT_BUTTON_Y = 130;
+    private static final int PLAY_BUTTON_Y = 220;
+    private static final int SETTINGS_BUTTON_WIDTH = 50;
+    private static final int SETTINGS_BUTTON_HEIGHT = 50;
+    private static final int SETTINGS_BUTTON_Y = 30;
+
+   
+    //Textures and skins
+    private Texture exitButtonActive;
+    private Texture exitButtonInactive;
+    private Texture playButtonActive;
+    private Texture playButtonInactive;
+    private Texture settingsButtonActive;
+    private Texture settingsButtonInactive;
+    private Texture backgroundTexture = new Texture("assets/clouds.jpg");
+
+    //Lables
+    private Label settings;
+
+    // other 
+    GolfGame game;
+    private Stage stage;
+    private Skin skin = new Skin(Gdx.files.internal("assets/skins/visui/assets/uiskin.json"));
+    
+    public MainMenu (GolfGame game) {
+       this.game = game;
+       stage = new Stage(new StretchViewport(GolfGame.WIDTH, GolfGame.HEIGHT));
+       Gdx.input.setInputProcessor(stage);
+       exitButtonActive = new Texture("assets/exit_button_active.jpg");
+       exitButtonInactive = new Texture("assets/exit_button_inactive.jpg");
+       playButtonActive = new Texture("assets/play_button_active.png");
+       playButtonInactive = new Texture("assets/play_button_inactive.png");
+       settingsButtonActive = new Texture("assets/settingsactive.png");
+       settingsButtonInactive = new Texture("assets/settingsinactive.png");
+       setupsettingslabel();
+    }
+       
+
+    @Override
+    public void show() {
+       
+    }
+
+
+    @Override
+    public void render(float delta) {
+
+         // Clear the screen
+         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        
+         // Draw the background texture
+         game.batch.begin();
+         game.batch.draw(backgroundTexture, 0, 0, GolfGame.WIDTH, GolfGame.HEIGHT);
+ 
+         //handling exit button - exits game
+         int x = GolfGame.WIDTH / 2 - EXIT_BUTTON_WIDTH / 2;
+         if (Gdx.input.getX() < x + EXIT_BUTTON_WIDTH && Gdx.input.getX() > x && GolfGame.HEIGHT - Gdx.input.getY() < EXIT_BUTTON_Y + EXIT_BUTTON_HEIGHT && GolfGame.HEIGHT - Gdx.input.getY() > EXIT_BUTTON_Y) {
+             game.batch.draw(exitButtonActive, GolfGame.WIDTH / 2 - EXIT_BUTTON_WIDTH / 2, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+             if (Gdx.input.isTouched()) {
+                 Gdx.app.exit();
+             }
+         } else {
+         game.batch.draw(exitButtonInactive, GolfGame.WIDTH / 2 - EXIT_BUTTON_WIDTH / 2, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+         }
+ 
+         //handling play button - sets the screen to OptionsScreen
+         x = GolfGame.WIDTH / 2 - PLAY_BUTTON_WIDTH / 2;
+         if (Gdx.input.getX() < x + PLAY_BUTTON_WIDTH && Gdx.input.getX() > x && GolfGame.HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && GolfGame.HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y) {
+             game.batch.draw(playButtonActive, GolfGame.WIDTH / 2 - PLAY_BUTTON_WIDTH / 2, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
+             if (Gdx.input.isTouched()) {
+                 game.setScreen(new OptionsScreen(game));
+             }
+         } else {
+         game.batch.draw(playButtonInactive, GolfGame.WIDTH / 2 - PLAY_BUTTON_WIDTH / 2, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
+         }
+
+         //handling settings button - sets the screen to SettingsScreen
+         x = GolfGame.WIDTH / 2 - SETTINGS_BUTTON_WIDTH / 2;
+         if (Gdx.input.getX() < x + SETTINGS_BUTTON_WIDTH && Gdx.input.getX() > x && GolfGame.HEIGHT - Gdx.input.getY() < SETTINGS_BUTTON_Y + SETTINGS_BUTTON_HEIGHT && GolfGame.HEIGHT - Gdx.input.getY() > SETTINGS_BUTTON_Y) {
+             game.batch.draw(settingsButtonActive, GolfGame.WIDTH / 2 - SETTINGS_BUTTON_WIDTH / 2, SETTINGS_BUTTON_Y, SETTINGS_BUTTON_WIDTH, SETTINGS_BUTTON_HEIGHT);
+             if (Gdx.input.isTouched()) {
+                 game.setScreen(new SettingsScreen(game));
+             }
+         } else {
+         game.batch.draw(settingsButtonInactive, GolfGame.WIDTH / 2 - SETTINGS_BUTTON_WIDTH / 2, SETTINGS_BUTTON_Y, SETTINGS_BUTTON_WIDTH, SETTINGS_BUTTON_HEIGHT);
+         }
+
+        
+         game.batch.end();
+ 
+         // Draw the stage
+         stage.act(Gdx.graphics.getDeltaTime());
+         stage.draw();
+     }
+
+    // method for setting up settings button 
+    public void setupsettingslabel() {
+        settings = new Label("SETTINGS", skin);
+        settings.setColor(Color.DARK_GRAY);
+        settings.setPosition(GolfGame.WIDTH / 2 - 33, SETTINGS_BUTTON_Y - 14);
+        settings.setSize(10, 5);
+        stage.addActor(settings);
+
+
+
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+        
+    }
+
+
+    @Override
+    public void pause() {
+        
+    }
+
+
+    @Override
+    public void resume() {
+        
+    }
+
+
+    @Override
+    public void hide() {
+    }
+
+
+    @Override
+    public void dispose() {
+       
+    }
+}
+
